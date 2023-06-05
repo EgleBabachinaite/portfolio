fbd79772139dbd08b7a0acde65b47bd0b2ee3e27
Predator <eglebabachinaite123@gmail.com>
2023-06-05 13:33:41 +0300

    Added a short description on the following projects on python

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ with col2:
     Information will be soon... 
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python.
+Feel free to contact me!
+"""
+st.write(content2)
