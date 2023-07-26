c6eef3b1e61e5270ad756defc99a7a16d5c3e3b2
Predator <eglebabachinaite123@gmail.com>
2023-07-26 17:28:32 +0300

    corrected sending email issues

--- send_email.py
+++ send_email.py
@@ -2,12 +2,18 @@ import smtplib, ssl
 import os
 
 
+def readpass():
+    with open("pass.txt", "r") as f:
+        content = f.read()
+        return content
+
+
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465  # For SSL
 
     username = "eglebabachinaite123@gmail.com"
-    password = os.getenv("PASSWORD")
+    password = readpass()
 
     receiver = "eglebabachinaite123@gmail.com"
     context = ssl.create_default_context()
