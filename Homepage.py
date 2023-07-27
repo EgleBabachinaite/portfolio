34fe29223c05ea0342b2024229cbbef988df24e7
Predator <eglebabachinaite123@gmail.com>
2023-07-27 18:02:32 +0300

    Added an introduction text

--- Homepage.py
+++ Homepage.py
@@ -11,13 +11,28 @@ with col1:
 with col2:
     st.title("Eglė Babachinaitė")
     content = """
-    Information will be soon... 
+    I am a Python enthusiast with a heart that beats for crafting elegant and efficient solutions 
+    through programming. Python, to me, is more than just a language; it's an art form that empowers 
+    me to bring ideas to life and make the impossible, possible. 
+    
+    As I seek to embark on my journey as a Python developer, I don't merely want to stand out; I 
+    strive to illuminate the way for innovation and excellence. My projects are a testament to my 
+    commitment to continuous growth and learning. I am ready to embrace challenges, collaborate with 
+    like-minded visionaries, and make a significant impact in the Python-powered world. 
+    
+    Currently, I find myself weaving the strands of imagination into reality as I create a 
+    mesmerizing Unicorn plush toys e-shop with Django. Combining my love for Python with the power 
+    of Django, I aim to create an enchanting user experience where the magic of technology meets 
+    the charm of creativity.
+    
+    With every line of code, I delve into a world of limitless possibilities, and my journey has 
+    been adorned with captivating projects that reflect my dedication.  
     """
     st.info(content)
 
 content2 = """
-Below you can find some of the apps I have built in Python.
-Feel free to contact me!
+Below you can find projects I have built in Python. Let's embark on this incredible journey 
+together, driven by innovation and empowered by the magic of Python.
 """
 st.write(content2)
 
--- pages/Certificates.py
+++ pages/Certificates.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
+
 content = """
-    Bellow you can review my current certificates in programming field both in 
-    Python and Javascript
+    Below you can review my current certificates in the programming field both in Python and Javascript
     """
 
-st.text(content)
-st.image("images/skillup_certificate.png", width=700)
-st.image("images/udemy_certificate.png", width=700)
-st.image("images/javascript_certificate.jpg", width=700)
+st.write(content)
+st.image("images/skillup_certificate.png", width=800)
+st.image("images/udemy_certificate.png", width=800)
+st.image("images/javascript_certificate.jpg", width=800)
